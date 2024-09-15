590e58ee07291ab03ecccf1e00e0364689c2628c
Braden Hilton <hiltonbraden@gmail.com>
2024-09-15 22:45:43 +0100

    [bntnews] Add article extractor

---
+++ extractor/bntnews.py
@@ -0,0 +1,88 @@
+"""Extractors for https://www.bntnews.co.kr/"""
+
+import json
+import re
+from http import HTTPStatus
+
+from gallery_dl import exception, text
+from gallery_dl.extractor.common import Extractor, Message
+
+BASE_PATTERN = r"(?:https?://)?www\.bntnews\.co\.kr"
+
+
+class BntnewsExtractor(Extractor):
+    """Base class for bntnews article extractors"""
+
+    category = "bntnews"
+    root = "https://www.bntnews.co.kr"
+
+    def _get_best_image_url(self, url):
+        return re.sub(
+            r"^(?:https?:)?//[^/]+/(data/bnt/)(?:cache|image)/(\d{4}/\d{2}/\d{2}/bnt\d+)\.\d+x\.\d+\.",
+            r"https://www.bntnews.co.kr/\1image/\2.",
+            url,
+        )
+
+    def _call(self, url, params=None):
+        if params is None:
+            params = {}
+        while True:
+            response = self.request(url, params=params, fatal=None, allow_redirects=False)
+            if response.status_code < HTTPStatus.MULTIPLE_CHOICES:
+                return response.text
+            if response.status_code == HTTPStatus.UNAUTHORIZED:
+                raise exception.AuthenticationError from None
+            if response.status_code == HTTPStatus.FORBIDDEN:
+                raise exception.AuthorizationError from None
+            if response.status_code == HTTPStatus.NOT_FOUND:
+                raise exception.NotFoundError(self.subcategory) from None
+            self.log.debug(response.text)
+            msg = "Request failed"
+            raise exception.StopExtraction(msg)
+
+
+class BntnewsArticleExtractor(BntnewsExtractor):
+    """Extractor for articles on www.bntnews.co.kr"""
+
+    subcategory = "article"
+    filename_fmt = "{article_id}_{filename}.{extension}"
+    directory_fmt = ("{category}", "{article_id}")
+    archive_fmt = "{article_id}_{filename}_{num}"
+    pattern = BASE_PATTERN + r"/(?:[^/]+/)?article/view/(bnt\d+)"
+    example = "https://www.bntnews.co.kr/Photo/article/view/bnt12345678910"
+
+    def __init__(self, match):
+        BntnewsExtractor.__init__(self, match)
+        self.article_id = match.group(1)
+        self.post_url = match.group(0)
+
+    def metadata(self, page):
+        json_data = json.loads(text.extr(page, '<script type="application/ld+json">', "</script>"))
+        return {
+            "title": text.unescape(json_data.get("headline")),
+            "author": json_data.get("author", {}).get("name"),
+            "date": text.parse_datetime(
+                json_data.get("datePublished"),
+                format="%Y-%m-%d %H:%M:%S%z",
+            ),
+            "article_id": self.article_id,
+            "post_url": json_data.get("mainEntityOfPage", {}).get("@id"),
+        }
+
+    def items(self):
+        page = self._call(self.post_url)
+        data = self.metadata(page)
+
+        article_content = text.extr(page, '<div class="content">', '<div class="copyright">')
+        urls = [
+            self._get_best_image_url(text.extr(image, 'src="', '"'))
+            for image in text.extract_iter(article_content, "<img", ">")
+        ]
+
+        yield Message.Directory, data
+
+        for data["num"], url in enumerate(urls, 1):
+            image = {"url": url}
+            data["image"] = image
+            text.nameext_from_url(text.unquote(url), data)
+            yield Message.Url, url, data
---
+++ test/results/bntnews.py
@@ -0,0 +1,37 @@
+from extractor import bntnews
+
+IMAGE_URL_PATTERN = r"(?i)https://www\.bntnews\.co\.kr/data/bnt/image/\d+/\d+/\d+/.*\.(?:gif|jpe?g|png|webp)"
+
+
+__tests__ = (
+    {
+        "#url": "https://www.bntnews.co.kr/article/view/bnt202409150018",
+        "#category": ("", "bntnews", "article"),
+        "#class": bntnews.BntnewsArticleExtractor,
+        "#pattern": IMAGE_URL_PATTERN,
+        "#count": 1,
+        "title": "‘영탁쇼’ 영탁, 관객 사연에 눈시울 붉혔다",
+        "author": "송미희",
+        "date": "dt:2024-09-15 02:38:13",
+    },
+    {
+        "#url": "https://www.bntnews.co.kr/Brand%20News/article/view/bnt202409120086",
+        "#category": ("", "bntnews", "article"),
+        "#class": bntnews.BntnewsArticleExtractor,
+        "#pattern": IMAGE_URL_PATTERN,
+        "#count": 2,
+        "title": "이가자헤어비스, 싱가포르 ‘2024 KAO SALON ASIA EXPERIENCE 갈라쇼’ 무대 서다",
+        "author": "정혜진",
+        "date": "dt:2024-09-14 00:00:01",
+    },
+    {
+        "#url": "https://www.bntnews.co.kr/Photo/article/view/bnt202409130107",
+        "#category": ("", "bntnews", "article"),
+        "#class": bntnews.BntnewsArticleExtractor,
+        "#pattern": IMAGE_URL_PATTERN,
+        "#count": 1,
+        "title": "후쿠모토 타이세이 '팬심 자극하는 스윗한 보이스' (일본 전국 투어 콘서트 '욘모지) [포토]",
+        "author": "김도윤",
+        "date": "dt:2024-09-13 09:05:04",
+    },
+)
