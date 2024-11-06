f8b987956e6ff58c4ccc0d55a452cb4a55407059
Braden Hilton <hiltonbraden@gmail.com>
2024-11-06 23:55:06 +0000

    [osen] Make www subdomain optional

--- test/results/osen.py
+++ test/results/osen.py
@@ -13,4 +13,13 @@ __tests__ = (
         "title": "(여자)아이들 미연 '사랑스럽게'",
         "date": "dt:2024-04-18 16:32:00",
     },
+    {
+        "#url": "https://osen.co.kr/article/G1112452170",
+        "#category": ("", "osen", "article"),
+        "#class": osen.OsenArticleExtractor,
+        "#pattern": IMAGE_URL_PATTERN,
+        "#count": 1,
+        "title": "에스타 윈터, 미소와 함께 퇴장",
+        "date": "dt:2024-11-06 11:38:00",
+    },
 )
