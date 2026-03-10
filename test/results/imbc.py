b22e0d2f020a1896573397644ba8c2532bbcbf93
Braden Hilton <hiltonbraden@gmail.com>
2026-03-10 23:28:37 +0000

    [imbc] remove adenews extractor

--- test/results/imbc.py
+++ test/results/imbc.py
@@ -7,15 +7,6 @@ IMAGE_URL_PATTERN = (
 
 __tests__ = (
     {
-        "#url": "https://adenews.imbc.com/M/Detail/431009",
-        "#category": ("", "imbc", "adenews-article"),
-        "#class": imbc.ImbcAdenewsArticleExtractor,
-        "#pattern": IMAGE_URL_PATTERN,
-        "#count": 40,
-        "title": "[움짤] 아이브 안유진, 숨 참고 럽럽~",
-        "date": "dt:2024-09-22 17:11:00",
-    },
-    {
         "#url": "https://enews.imbc.com/News/RetrieveNewsInfo/431009",
         "#category": ("", "imbc", "enews-article"),
         "#class": imbc.ImbcEnewsArticleExtractor,
