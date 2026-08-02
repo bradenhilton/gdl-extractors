598868afd018e422f6d8f0b3135f265dbfa5a6d0
Braden Hilton <hiltonbraden@gmail.com>
2026-08-02 05:26:16 +0100

    [sbsprograms] add random sleep for tests under CI

--- test/_env.py
+++ test/_env.py
@@ -5,3 +5,7 @@ IS_CI = "CI" in os.environ and "GITHUB_ACTIONS" in os.environ
 
 def ci_skip(reason):
     return reason if IS_CI else None
+
+
+def ci_sleep(ci_duration, extractor):
+    return ci_duration if IS_CI else extractor.request_interval
